7e64511ff8844ed2d8d5c3c27298f21666def9f2
anishneema <133931817+anishneema@users.noreply.github.com>
2026-07-06 13:21:33 -0700

    Add 90_subsets_ii.py - LeetCode Problem 90

---
+++ 90_subsets_ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(curr, i):
+
+            for j in range(i, len(nums)):
+
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+                
+                curr.append(nums[j])
+                ans.append(curr[:])
+                backtrack(curr, j+1)
+                curr.pop()
+            
+        nums.sort()
+        ans = [[]]
+        backtrack([], 0)
+        return ans
