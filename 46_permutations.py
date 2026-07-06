e0bdf6c4494745b6e16bc73c3f8d2de9c2fc3a98
anishneema <133931817+anishneema@users.noreply.github.com>
2026-07-06 12:18:19 -0700

    Update 46_permutations.py - LeetCode Problem 46

--- 46_permutations.py
+++ 46_permutations.py
@@ -1,19 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def backtrack(curr):
+        def backtrack(curr, pick):
 
             if len(curr) == len(nums):
                 ans.append(curr[:])
                 return
             
             for j in range(len(nums)):
-                if nums[j] not in curr:
+                if not pick[j]:
                     curr.append(nums[j])
-                    backtrack(curr)
+                    pick[j] = True
+                    backtrack(curr, pick)
                     curr.pop()
+                    pick[j] = False
             
         
         ans = []
-        backtrack([])
+        backtrack([], [False]*len(nums))
         return ans
