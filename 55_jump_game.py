af54c576945ef4688577908dce8fd9069a6aa74d
anishneema <133931817+anishneema@users.noreply.github.com>
2026-07-21 21:42:39 -0400

    Add 55_jump_game.py - LeetCode Problem 55

---
+++ 55_jump_game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+    
+        
+        far = 0
+
+        for i, n in enumerate(nums):
+
+            if far < i:
+                return False
+            
+            far = max(far, i + n)
+
+            if far >= len(nums) -1:
+                return True
